b8bec7422fe06f9a07c9917d24ba2f61e9f64610
Den Denowich <denowich@gmail.com>
2024-08-16 17:35:56 +0300

    Lesson - 21_(added in conponent Dialogs names and messages)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,5 +2,22 @@ import React from 'react';
 import style from './Dialogs.module.css';
 
 export const Dialogs = () => {
-      return <div className={style.dialogs}>Dialogs</div>;
+      return (
+            <div className={style.dialogs}>
+                  <div className={style.dialogsItems}>
+                        <div className={style.dialog}>Den</div>
+                        <div className={style.dialog}>Rex</div>
+                        <div className={`${style.dialog} ${style.active}`}>
+                              Danya
+                        </div>
+                        <div className={style.dialog}>Vika</div>
+                        <div className={style.dialog}>Sanya</div>
+                  </div>
+                  <div className={style.messagesItems}>
+                        <div className={style.dialog}>Hi!</div>
+                        <div className={style.dialog}>How are you?</div>
+                        <div className={style.dialog}>Yoooo!!!</div>
+                  </div>
+            </div>
+      );
 };
